6f20d77dfea14c9f486f04c4c0fb36d17f4ff8b2
Rohit Saini <sketchlearn7@gmail.com>
2020-11-08 01:15:40 +0530

    simple Post api added

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,7 +5,25 @@ func routes(_ app: Application) throws {
         return "It works!"
     }
 
-    app.get("welcome") { req -> String in
-        return "Welcome to vapor world"
+    app.on(.GET, "welcome",":team") { req -> String in
+        guard let team = req.parameters.get("team") else{
+            return "Error in team"
+        }
+        return "Welcome \(team) to vapor world"
     }
+
+    app.on(.POST, "user","login") { req -> User in
+        let user = try req.content.decode(User.self)
+        print(user)
+        return User(email: user.email,status: "\(user.email!)) logged in successfully!",code:100)
+    }
+
+    
+}
+
+struct User: Content {
+    var email: String?
+    var password: String?
+    var status:String?
+    var code: Int?
 }
